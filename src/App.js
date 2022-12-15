75f30550bab05059a780028d43841dde4b49daa0
Vishnu Tej Nanda Kumar <Vishnu.Nanda.Kumar@smu.ca>
2022-12-14 21:01:23 -0400

    Login Signup page redo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './modules/Home';
 import { Header } from './modules/Header';
 import {Product} from './modules/Product';
+import {LoginPage} from './modules/AuthModule/LoginPage';
+import {RegisterPage} from './modules/AuthModule/RegisterPage';
+import { LandingPage } from './modules/AuthModule/LandingPage';
+// import {ForgotPasswordPage} from './modules/AuthModule/ForgotPasswordPage';
 
 const App = () => {
   return (
@@ -14,6 +18,10 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/product" element={<Product />} />
+          <Route exact path="/login" element={ <LoginPage/> } />
+          <Route path="/register" element={ <RegisterPage/> } />
+          <Route path="/landingPage" element={ <LandingPage/> } />
+          {/* <Route path="/forget-password" element={ <ForgotPasswordPage/> } /> */}
         </Routes>
       </BrowserRouter>
     </>
